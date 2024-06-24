8c47cfbf610d74684a4c1813ec2a6afb08ca8cc2
axellokrantz <axel.lokrantz@gmail.com>
2024-06-24 21:25:07 +0200

    2/75

--- 1/1.cs
+++ 1/1.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+public class Solution
+{
+
+    public string MergeAlternately(string word1, string word2)
+    {
+        StringBuilder result = new StringBuilder();
+        int j = 0;
+
+        if (word1.Length >= word2.Length)
+        {
+            for (int i = 0; i < word2.Length; i++)
+            {
+                result.Append(word1[i]);
+                result.Append(word2[i]);
+                j++;
+            }
+            result.Append(word1.Substring(j));
+        }
+        else
+        {
+            for (int i = 0; i < word1.Length; i++)
+            {
+                result.Append(word1[i]);
+                result.Append(word2[i]);
+                j++;
+            }
+            result.Append(word2.Substring(j));
+        }
+        return result.ToString();
+    }
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.MergeAlternately("1", "2"));
+    }
+}
--- 2/2.cs
+++ 2/2.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2");
+﻿using System.Numerics;
+
+public class Solution
+{
+
+    public string GcdOfStrings(string str1, string str2)
+    {
+        int len1 = str1.Length;
+        int len2 = str2.Length;
+        int gcd = (int)BigInteger.GreatestCommonDivisor(len1, len2);
+
+        string substr = str1.Substring(0, gcd);
+        string str = str1 + str2;
+
+        for (int i = 0; i < str.Length; i += gcd)
+        {
+            if (str.Substring(i, gcd) != substr)
+            {
+                return "";
+            }
+        }
+        return substr;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.GcdOfStrings("1", "2"));
+    }
+}
