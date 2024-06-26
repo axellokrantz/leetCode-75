de3a727535bfc3dd8c5c744f3540d0a232474362
axellokrantz <axel.lokrantz@gmail.com>
2024-06-26 22:06:10 +0200

    6/75

---
+++ 6/6.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string trimmedInput = s.TrimStart().TrimEnd();
+        string singelSpaced = Regex.Replace(trimmedInput, @"\s+", " ");
+        string[] words = singelSpaced.Split(' ');
+        Array.Reverse(words);
+        string result = string.Join(" ", words);
+        return result;
+    }
+
+
+    static void Main(string[] args)
+    {
+        string str = "hello  lol";
+        Solution s = new Solution();
+        Console.WriteLine(s.ReverseWords(str));
+    }
+}
