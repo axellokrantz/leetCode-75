9b18cf46cb82f490fbe21450534d8dfa0fb603ba
axellokrantz <axel.lokrantz@gmail.com>
2024-06-28 13:27:06 +0200

    11/75

---
+++ 11/11.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if(string.IsNullOrEmpty(s)){
+            return true;
+        }
+
+        int j = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i] == s[j])
+            {
+                j++;
+            }
+
+            if (j == s.Length)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "axc";
+        string t = "ahbgdc";
+        Solution solution = new Solution();
+        Console.Write(solution.IsSubsequence(s, t));
+    }
+}
