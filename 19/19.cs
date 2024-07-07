491a311d73e6c390c09a471314acbdb7e1060906
axellokrantz <axel.lokrantz@gmail.com>
2024-07-07 22:58:03 +0200

    21/75

--- 19/19.cs
+++ 19/19.cs
@@ -1,16 +1,38 @@
-﻿// public class Solution
-// {
-//     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
-//     {
-//         IList<IList<int>> list = new List<IList<int>>();
-//         return list;
-//     }
+﻿public class Solution
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        IList<int> distinctNums1 = new List<int>();
+        IList<int> distinctNums2 = new List<int>();
+        IList<IList<int>> result = new List<IList<int>>();
 
-//     static void Main(string[] args)
-//     {
-//         int[] nums = [1, 7, 3, 6, 5, 6];
-//         int[] nums2 = [1, 7, 3, 6, 5, 6];
-//         Solution solution = new Solution();
-//         Console.WriteLine(solution.FindDifference(nums));
-//     }
-// }
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            if(!nums2.Contains(nums1[i]) && !distinctNums1.Contains(nums1[i]))
+            {
+                distinctNums1.Add(nums1[i]);
+            }
+        }
+
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            if(!nums1.Contains(nums2[i]) && !distinctNums2.Contains(nums2[i]))
+            {
+                distinctNums2.Add(nums2[i]);
+            }
+        }
+
+        result.Add(distinctNums1);
+        result.Add(distinctNums2);
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = [1, 2, 3];
+        int[] nums2 = [2, 4, 6];
+        Solution solution = new Solution();
+        Console.WriteLine(solution.FindDifference(nums, nums2));
+    }
+}
---
+++ 21/21.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public bool CloseStrings(string word1, string word2) {
+        
+        if(word1.Length != word2.Length)
+        {
+            return false;
+        }
+
+        int[] frequency1 = new int[26];
+        int[] frequency2 = new int[26];
+
+        foreach (char c in word1)
+        {
+            frequency1[c - 'a']++;
+        }
+
+        foreach (char c in word2)
+        {
+            frequency2[c - 'a']++;
+        }
+
+        Array.Sort(frequency1);
+        Array.Sort(frequency2);
+
+        return Enumerable.SequenceEqual(frequency1, frequency2);
+    }
+
+    static void Main(string[] args)
+    {
+        string word1 = "abc";
+        string word2 = "bca";
+        Solution solution = new Solution();
+        Console.WriteLine(solution.CloseStrings(word1, word2));
+    }
+}
